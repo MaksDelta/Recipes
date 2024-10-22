f016e0b67238e31253cf35ec518c318b94d03e44
Maks Delta <mc.pullupenko@gmail.com>
2024-10-23 01:11:54 +0300

    add action

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { test, expect } from '../eslint.config.mjs'
 import App from './App';
 
 test('renders learn react link', () => {
